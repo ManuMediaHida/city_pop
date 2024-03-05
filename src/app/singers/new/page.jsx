f2ebcf662c79e34d418dadd46b226bb98af9fd2d
Manuel Mediavilla Hidalgo <mmediavillahidalgot@gmail.com>
2024-03-06 00:50:15 +0100

    Style fixes
    Song form with new styles

--- src/app/singers/new/page.jsx
+++ src/app/singers/new/page.jsx
@@ -32,8 +32,8 @@ function NewSingerPage() {
 
   return (
     <div className="container mx-auto p-4">
-      <h2 className="text-2xl font-semibold my-4">Nuevo Cantante</h2>
-      {successMessage && <div className="text-green-500">{successMessage}</div>}
+      <h2 className="text-2xl font-semibold my-4 text-white">Nuevo Cantante</h2>
+      {successMessage && <div className="text-green-500 bg-white">{successMessage}</div>}
       {errorMessage && <div className="text-red-500">{errorMessage}</div>}
       <FormSinger onSubmit={handleSubmit} />
       <Link href="/singers" className="mt-4 inline-block bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 transition-colors">
