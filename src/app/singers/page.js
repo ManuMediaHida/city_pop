449bcdb8bf8cc5a99d2f7d60d95e94b4d2342ad5
Manuel Mediavilla Hidalgo <mmediavillahidalgot@gmail.com>
2024-03-05 18:45:06 +0100

    Force Dynamic Singers
    Arreglo de force dynamic en singers

--- src/app/singers/page.js
+++ src/app/singers/page.js
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import SingerCard from '@/components/SingerCard'
 import { getAllSingers } from '@/libs/actions'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
     const singers = await getAllSingers();
   
